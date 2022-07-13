e96fbed0ea4a240027a38165b988b6dc09e224a4
Islam Tarek <islamtarek0550@gmail.com>
2022-07-13 10:43:27 +0200

    Add comments for APIs

--- MCAL_GPIO/mcal_gpio.c
+++ MCAL_GPIO/mcal_gpio.c
@@ -7,6 +7,13 @@
 /* includes */
 #include "mcal_gpio.h"
 
+/**
+ * @brief  This function is used to set the pin direction 
+ * @param gpio_x    : The used port (eg: MCAL_GPIO_A)
+ * @param pin_x     : The pin used  (eg: MCAL_PIN_0 , MCAL_PIN_0 | MCAL_PIN_1, MCAL_PIN_ALL)
+ * @param direction : The direction of the pin (eg: PIN_OUTPUT)
+ * @return MCAL_OK in case everything is done properly and MCAL_FAIL otherwise.
+ */
 mcal_err_t mcal_gpio_set_pin_direction(mcal_gpio_t gpio_x, mcal_gpio_pin_t pin_x, mcal_gpio_pin_direction_t direction)
 {
     switch(direction)
@@ -29,6 +36,13 @@ mcal_err_t mcal_gpio_set_pin_direction(mcal_gpio_t gpio_x, mcal_gpio_pin_t pin_x
     return MCAL_OK;
 }
 
+/**
+ * @brief This function is used to set the pin level in case the pin direction is set to PIN_OUTPUT 
+ * @param gpio_x : The used port (eg: MCAL_GPIO_A)
+ * @param pin_x  : The pin used  (eg: MCAL_PIN_0 , MCAL_PIN_0 | MCAL_PIN_1, MCAL_PIN_ALL)
+ * @param level  : The level of the pin (eg: PIN_HIGH)
+ * @return MCAL_OK in case everything is done properly and MCAL_FAIL otherwise.
+ */
 mcal_err_t mcal_gpio_set_pin_level(mcal_gpio_t gpio_x, mcal_gpio_pin_t pin_x, mcal_gpio_pin_level_t level)
 {
     switch(level)
@@ -46,6 +60,14 @@ mcal_err_t mcal_gpio_set_pin_level(mcal_gpio_t gpio_x, mcal_gpio_pin_t pin_x, mc
     }
     return MCAL_OK;
 }
+
+/**
+ * @brief This function is used to get the pin level in case the pin direction is set to PIN_INPUT or PIN_INPUT_PU
+ * @param gpio_x : The used port (eg: MCAL_GPIO_A)
+ * @param pin_x  : The pin used  (eg: MCAL_PIN_0 , MCAL_PIN_0 | MCAL_PIN_1, MCAL_PIN_ALL)
+ * @param level  : The level of the pin (eg: PIN_HIGH)
+ * @return MCAL_OK in case everything is done properly and MCAL_FAIL otherwise. 
+ */
 mcal_err_t mcal_gpio_get_pin_level(mcal_gpio_t gpio_x, mcal_gpio_pin_t pin_x, mcal_gpio_pin_level_t* level)
 {
     if(gpio_get_pin_level(gpio_x , pin_x, level) != DRIVER_OK)
@@ -53,6 +75,12 @@ mcal_err_t mcal_gpio_get_pin_level(mcal_gpio_t gpio_x, mcal_gpio_pin_t pin_x, mc
     return MCAL_OK;
 }
 
+/**
+ * @brief This function is used to toggle pin state in case the pin direction is set to PIN_OUTPUT
+ * @param gpio_x : The used port (eg: MCAL_GPIO_A)
+ * @param pin_x  : The pin used  (eg: MCAL_PIN_0 , MCAL_PIN_0 | MCAL_PIN_1, MCAL_PIN_ALL)
+ * @return MCAL_OK in case everything is done properly and MCAL_FAIL otherwise. 
+ */
 mcal_err_t mcal_gpio_toggle_pin(mcal_gpio_t gpio_x, mcal_gpio_pin_t pin_x)
 {
     if(gpio_toggle_pin(gpio_x, pin_x) != DRIVER_OK)
