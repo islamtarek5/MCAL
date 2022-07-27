ddc8d0fb502414bc58f145889af85b1fb6abd084
Islam Tarek <islamtarek0550@gmail.com>
2022-07-27 15:09:27 +0200

    Add set source current and set wake up state APIs

--- MCAL_GPIO/mcal_gpio.c
+++ MCAL_GPIO/mcal_gpio.c
@@ -1,7 +1,7 @@
 /*********************************************************************************************************************************************/
 /*                                                      Author      : Islam Tarek                                                            */
 /*                                                      SW_Module   : MCAL GPIO                                                              */
-/*                                                      Last_update : 13/7/2022                                                              */  
+/*                                                      Last_update : 27/7/2022                                                              */  
 /*********************************************************************************************************************************************/
 
 /* includes */
@@ -87,3 +87,82 @@ mcal_err_t mcal_gpio_toggle_pin(mcal_gpio_t gpio_x, mcal_gpio_pin_t pin_x)
         return MCAL_FAIL;
     return MCAL_OK;
 }
+/**
+ * @brief This function is used to Enable / Disable Wake up for the microcotroller by GPIO 
+ * @param pin_x : The pin used  (eg: MCAL_PIN_0 , MCAL_PIN_1, ...)
+ * @param state : The state of wake up (eg : WAKE_UP_DIABLE or WAKE_UP_ENABLE)
+ * @return MCAL_OK in case everything is done properly and MCAL_FAIL otherwise. 
+ */
+mcal_err_t mcal_gpio_set_wake_up_state(mcal_gpio_pin_t pin_x, mcal_gpio_wake_up_state_t state)
+{
+    switch (state)
+    {
+    case WAKE_UP_DISABLE:
+        gpio_disable_wake_up(pin_x);
+        break;
+    case WAKE_UP_ENABLE:
+        gpio_enable_wake_up(pin_x);
+        break;
+    default:
+        return MCAL_FAIL;
+    }
+    return MCAL_OK;
+}
+
+/**
+ * @brief This function is used to set output current source of a specific pin
+ * @param gpio_x : The used port (eg: MCAL_GPIO_A)
+ * @param pin_x  : The pin used  (eg: MCAL_PIN_0 , MCAL_PIN_0 | MCAL_PIN_1, MCAL_PIN_ALL)
+ * @param voltage: The Voltage source to microcontroller 
+ * @param current: The disred output current 
+ * @return MCAL_OK in case everything is done properly and MCAL_FAIL otherwise. 
+ */
+mcal_err_t mcal_gpio_set_src_current(mcal_gpio_t gpio_x, mcal_gpio_pin_t pin_x, mcal_voltage_src_t voltage, mcal_current_t current)
+{
+    switch (voltage)
+    {
+    case VOLTAGE_SRC_3_V:
+        switch (current)
+        {
+        case CURRNET_SRC_1_5_uA:
+            gpio_set_src_current(gpio_x, pin_x, SRC_CURRNET_1_5_uA_3_V);
+            break;
+        case CURRNET_SRC_2_0_uA:
+            gpio_set_src_current(gpio_x, pin_x, SRC_CURRNET_2_0_uA_3_V);
+            break;
+        case CURRNET_SRC_3_0_uA:
+            gpio_set_src_current(gpio_x, pin_x, SRC_CURRNET_3_0_uA_3_V);
+            break;
+        case CURRNET_SRC_6_0_uA:
+            gpio_set_src_current(gpio_x, pin_x, SRC_CURRNET_6_0_uA_3_V);
+            break;
+        default:
+            return MCAL_FAIL;
+        }
+        break;
+    case VOLTAGE_SRC_5_V:
+        switch (current)
+        {
+        case CURRNET_SRC_3_6_uA:
+            gpio_set_src_current(gpio_x, pin_x, SRC_CURRNET_3_6_uA_5_V);
+            break;
+        case CURRNET_SRC_6_0_uA:
+            gpio_set_src_current(gpio_x, pin_x, SRC_CURRNET_6_0_uA_5_V);
+            break;
+        case CURRNET_SRC_8_0_uA:
+            gpio_set_src_current(gpio_x, pin_x, SRC_CURRNET_8_0_uA_5_V);
+            break;
+        case CURRNET_SRC_18_0_uA:
+            gpio_set_src_current(gpio_x, pin_x, SRC_CURRNET_18_0_uA_5_V);
+            break;
+        default:
+            return MCAL_FAIL;
+        }
+        break;
+    default:
+            return MCAL_FAIL;
+    }
+
+    return MCAL_OK;
+}
+
--- MCAL_GPIO/mcal_gpio.h
+++ MCAL_GPIO/mcal_gpio.h
@@ -1,7 +1,7 @@
 /*********************************************************************************************************************************************/
 /*                                                      Author      : Islam Tarek                                                            */
 /*                                                      SW_Module   : MCAL GPIO                                                              */
-/*                                                      Last_update : 13/7/2022                                                              */  
+/*                                                      Last_update : 27/7/2022                                                              */  
 /*********************************************************************************************************************************************/
 
 #ifndef MCAL_GPIO_MCAL_GPIO_H_
@@ -9,7 +9,7 @@
 
 /* Includes */
 #include "../mcal.h"
-#include "../../drivers/Atmega32/GPIO/gpio.h" /* This will change depending on the MCU used*/
+#include "../../driver/HT66F489/GPIO/gpio.h" /* This will change depending on the MCU used*/
 
 /* MCAL GPIO ports */
 typedef enum
@@ -39,15 +39,37 @@ typedef uint8_t mcal_gpio_pin_direction_t;
 #define PIN_INPUT       1
 #define PIN_INPUT_PU    2
 
-/* MCAL GPIO pin level*/
+/* MCAL GPIO pin level */
 typedef uint8_t mcal_gpio_pin_level_t;
 #define PIN_HIGH        0
 #define PIN_LOW         1
+
+/* MCAL GPIO Wake up state */
+typedef uint8_t mcal_gpio_wake_up_state_t;
+#define WAKE_UP_DISABLE     0
+#define WAKE_UP_ENABLE      1
+
+/* MCAL GPIO Source Current */ 
+typedef uint8_t mcal_current_t;
+#define CURRNET_SRC_1_5_uA      0      /*  1.5 micro Ampere @ 3.0 volt              */
+#define CURRNET_SRC_2_0_uA      1      /*  2.0 micro Ampere @ 3.0 volt              */
+#define CURRNET_SRC_3_0_uA      2      /*  3.0 micro Ampere @ 3.0 volt              */ 
+#define CURRNET_SRC_3_6_uA      3      /*  3.6 micro Ampere @ 5.0 volt              */
+#define CURRNET_SRC_6_0_uA      4      /*  6.0 micro Ampere @ 3.0 volt or 5.0 volt  */ 
+#define CURRNET_SRC_8_0_uA      5      /*  8.0 micro Ampere @ 5.0 volt              */
+#define CURRNET_SRC_18_0_uA     6      /*  18.0 micro Ampere @ 5.0 volt             */
+
+/* MCAL GPIO Source Current */ 
+typedef uint8_t mcal_voltage_src_t;
+#define VOLTAGE_SRC_3_V         0
+#define VOLTAGE_SRC_5_V         1
 
 /* APIs*/
 mcal_err_t mcal_gpio_set_pin_direction  (mcal_gpio_t gpio_x, mcal_gpio_pin_t pin_x, mcal_gpio_pin_direction_t direction);
 mcal_err_t mcal_gpio_set_pin_level      (mcal_gpio_t gpio_x, mcal_gpio_pin_t pin_x, mcal_gpio_pin_level_t level);
 mcal_err_t mcal_gpio_get_pin_level      (mcal_gpio_t gpio_x, mcal_gpio_pin_t pin_x, mcal_gpio_pin_level_t* level);
 mcal_err_t mcal_gpio_toggle_pin         (mcal_gpio_t gpio_x, mcal_gpio_pin_t pin_x);
+mcal_err_t mcal_gpio_set_wake_up_state  (mcal_gpio_pin_t pin_x, mcal_gpio_wake_up_state_t state);
+mcal_err_t mcal_gpio_set_src_current    (mcal_gpio_t gpio_x, mcal_gpio_pin_t pin_x, mcal_voltage_src_t voltage, mcal_current_t current);
 
 #endif
